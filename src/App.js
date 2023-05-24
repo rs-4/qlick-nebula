ce8b6b023af43c663cf91c46fc3ea4aa59548eb7
Rayan Selmi <72416925+rs-4@users.noreply.github.com>
2023-05-24 17:27:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import embed from "./configure";
+import connect from "./connect";
+import { useEffect } from "react";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [n, setN] = useState();
+
+  useEffect(() => {
+    const app = connect({
+      url: "https://2w32pfh3l2b94yd.eu.qlikcloud.com",
+      webIntegrationId: "pJZiFzzGAFrWk0EtZ9qmvHZSP_ltp7SJ",
+      appId: "d711350f-0729-4b03-bb10-8de83c8cc4a7",
+    });
+  });
+
+  return <div></div>;
+};
 
 export default App;
---
+++ src/connect.js
@@ -0,0 +1,49 @@
+import enigma from "enigma.js";
+import schema from "enigma.js/schemas/12.170.2.json";
+
+async function getQCSHeaders({ webIntegrationId, url }) {
+  const response = await fetch(`${url}/api/v1/csrf-token`, {
+    headers: {
+      "qlik-web-integration-id": webIntegrationId,
+    },
+    credentials: "include",
+  });
+  if (response.status === 401) {
+    const loginUrl = new URL(`${url}/login`);
+    loginUrl.searchParams.append("returnto", window.location.href);
+    loginUrl.searchParams.append("qlik-web-integration-id", webIntegrationId);
+    window.location.href = loginUrl.href;
+    return undefined;
+  }
+  const crfsToken = response.headers.get("qlik-csrf-token");
+  return {
+    "qlik-web-integration-id": webIntegrationId,
+    "qlik-csrf-token": crfsToken,
+  };
+}
+
+async function getEnigmamApp({ host, appId, headers }) {
+  const params = Object.keys(headers)
+
+    .map((key) => `${key}=${encodeURIComponent(headers[key])}`)
+    .join("&");
+
+  const enigmaGlobal = await enigma
+
+    .create({
+      schema,
+      url: `wss://${host}/app/${appId}?${params}`,
+      createSocket: (url) => new WebSocket(url),
+    })
+    .open();
+
+  return enigmaGlobal.openDoc(appId);
+}
+
+async function connect({ url, webIntegrationId, appId }) {
+  const host = url.replace(/https?:\/\//, "").replace(/\/$/, "");
+  const headers = await getQCSHeaders({ webIntegrationId, url });
+  return getEnigmamApp({ host, appId, headers });
+}
+
+export default connect;
